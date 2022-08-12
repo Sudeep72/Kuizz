83cc5485697fa9453ed90bc341a3717dbd16e565
Sudeep <sudeep7217@gmail.com>
2022-08-12 23:01:05 +0530

    Initial commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginBottom: 10,
+      }}
+    >
+      Made with ♥ by{" "}
+      <a
+        href="https://sudeepdev.works"
+        style={{ cursor: "pointer" }}
+      >
+        Sudeep
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
